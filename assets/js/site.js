8226e5844de8b7349be841d8f214fee4a11c6307
Minh Pham <congminh.pham@mirumagency.com>
2019-05-23 00:35:09 +0700

    home page

---
+++ assets/js/site.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('.nav-icon').click(function() {
+        $(this).toggleClass('open');
+        $('.nav-mobile').toggleClass('open');
+        $('.over-lay').toggleClass('open');
+    });
+    $('.nav-mobile .icon-cancel').click(function() {
+        $('.nav-icon').removeClass('open');
+        $('.nav-mobile').removeClass('open');
+        $('.over-lay').removeClass('open');
+    });
+    $('.ic-arrow').click(function() {
+        $(this).parent().toggleClass('open');
+    });
+    $('.type-radio').click(function() {
+        $('.type-radio').removeClass('active');
+        $(this).addClass('active');
+    });
+
+});
